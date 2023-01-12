ea5431b3f17ee72e0eae8fdbdbdef1b8b0f59fc8
Hascyll <122144250+Hascyll@users.noreply.github.com>
2023-01-12 15:45:29 +0100

    Code cleanup

--- flaskeddit/user/routes.py
+++ flaskeddit/user/routes.py
@@ -1,12 +1,9 @@
 from flask import render_template, request
 
 from flaskeddit.user import user_blueprint, user_service
-from flaskeddit.models import AppUser
-from flaskeddit.config import Config
-from flask_login import current_user
-from flaskeddit import db
 
-import requests
+from flask_login import current_user
+
 
 @user_blueprint.route("/user/<string:username>", methods=['GET','POST'])
 def app_user(username):
@@ -15,28 +12,25 @@ def app_user(username):
     """
     if request.method == 'POST':
         try:
-            try:	
+            try:
                 content = request.json
-                print(content)
-                print(type(content))
-                print(content['action'])
                 try:
                     if content['user_name']:
                         print('Log: received data for user ' + str(current_user.username) + ', Pi Network Username: ' + str(content['user_name']))
-                        verifyWithPi(content)
-                except: 
+                        user_service.verifyWithPi(content)
+                except:
                     pass
                 try:
                     if str(content['action']) == 'approve' or str(content['action']) == 'complete':
-                        verifyPayments(content)
-                except: 
+                        user_service.verifyPayments(content)
+                except:
                     pass
             except:
                 pass
             try:
                 content = request.form['wallet']
                 if len(content) == 56 and content[0] == 'G':
-                    updateWallet(content)	
+                    user_service.updateWallet(content)
             except:
                 pass
         except:
@@ -47,46 +41,3 @@ def app_user(username):
     else:
         user, email, moderator = user_service.get_user(username)
         return render_template("user.html", app_user=user)
-
-def verifyWithPi(content):
-    """
-    Verifies the Pi Network data the user sent with the backend-API
-    User-content: [user_token, user_name, user_roles]
-    """
-    baseURL = str(Config.PLATFORM_API_URL)
-    user_name = content['user_name']
-    authentication = 'Bearer ' + content['user_token']
-    auth_bearer = {'Authorization':authentication}
-    request_verification = requests.get(baseURL + '/v2/me', headers = auth_bearer)
-    server_data = eval(request_verification.text)
-    print(server_data)
-    if server_data['username'] == content['user_name']:
-        app_user = AppUser.query.filter_by(username=current_user.username).first()
-        app_user.pi_username = server_data['username']
-        print('Log: found roles are ' + str(server_data['roles']))
-        if server_data['roles'][0] == 'moderator':
-            app_user.moderator = True
-        db.session.commit()
-     
-def verifyPayments(content):
-    """
-    Verifies the Pi Network data the user sent with the backend-API
-    User-content: [user_token, user_name, user_roles]
-    """
-    baseURL = str(Config.PLATFORM_API_URL)
-    authentication = 'Key ' + str(Config.PI_API_KEY)
-    header = {'Authorization': authentication}  
-    if str(content['action']) == 'approve':
-        url = baseURL + '/v2/payments/' + content['paymentId'] + '/approve'
-        data = {}
-    if str(content['action']) == 'complete':
-        url = baseURL + '/v2/payments/' + content['paymentId'] + '/complete'
-        data = {'txid': content['txid']}
-    
-    request_payment = requests.post(url, json=data, headers=header)
-
-
-def updateWallet(wallet):
-    app_user = AppUser.query.filter_by(username=current_user.username).first()
-    app_user.pi_wallet = wallet
-    db.session.commit()
