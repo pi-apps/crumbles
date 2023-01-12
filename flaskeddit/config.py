37ea15f85b8b3a4fff6250e2b96f3c592aa5ef25
Hascyll <122144250+Hascyll@users.noreply.github.com>
2023-01-12 07:09:54 +0100

    Added SALT for password-hashing
    This allows the backend to add a server-side salt to the user-passwords to avoid rainbow-table attacks.

--- flaskeddit/config.py
+++ flaskeddit/config.py
@@ -3,13 +3,18 @@ import os
 
 class Config:
     """Configuration settings for the Flaskeddit Flask app."""
-
+    
+    # Change this before using it
     SECRET_KEY = os.environ.get("SECRET_KEY", "placeholder-secret-key")
+    
+    # Server-side salt for password hashing - Change this before using it
+    Server_SALT = 'placeholder-secret-salt'
+    
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Get this on the Pi Developer Portal (develop.pi in the Pi Browser)
-    PI_API_KEY = '' #Add your API Key here
+    PI_API_KEY = '' #Add your SECRET API Key here
 
     # Platform API
     PLATFORM_API_URL = 'https://api.minepi.com'
