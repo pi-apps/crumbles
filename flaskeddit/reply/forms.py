a045be03d0a1236fb85c3f9769cb491e6f8c84b1
Hascyll <122144250+Hascyll@users.noreply.github.com>
2023-01-15 09:34:29 +0100

    Rich Text Editor for Posts

--- flaskeddit/reply/forms.py
+++ flaskeddit/reply/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import SubmitField, TextAreaField
 from wtforms.validators import DataRequired
-
+from flask_ckeditor import CKEditorField
 
 class ReplyForm(FlaskForm):
     """Form for creating and updating a reply."""
 
-    reply = TextAreaField("Reply", validators=[DataRequired()])
+    reply = CKEditorField("Reply", validators=[DataRequired()])
     submit = SubmitField("Submit")
