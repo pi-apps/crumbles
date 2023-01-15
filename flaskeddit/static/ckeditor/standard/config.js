bbbbaa0c1365e17f585b0acd70473e42b63c87f8
Hascyll <122144250+Hascyll@users.noreply.github.com>
2023-01-15 09:40:27 +0100

    Rich Text Editor for Posts

---
+++ flaskeddit/static/ckeditor/standard/config.js
@@ -0,0 +1,26 @@
+/**
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	config.toolbarGroups = [
+		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+		{ name: 'links', groups: [ 'links' ] },
+		{ name: 'insert', groups: [ 'insert' ] },
+		{ name: 'forms', groups: [ 'forms' ] },
+		{ name: 'tools', groups: [ 'tools' ] },
+		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'others', groups: [ 'others' ] },
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+		{ name: 'styles', groups: [ 'styles' ] },
+		{ name: 'colors', groups: [ 'colors' ] },
+		'/',
+		'/',
+		{ name: 'about', groups: [ 'about' ] }
+	];
+
+	config.removeButtons = 'Subscript,Superscript,Paste,PasteText,PasteFromWord,Scayt,Anchor,Table,Image,Maximize,Source,RemoveFormat,About';
+};
